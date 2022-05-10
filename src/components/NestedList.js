dcee679b43594c8200264bdfe56de23de174fd63
Mateusz <mateusz.grzybowski98@gmail.com>
2022-05-10 10:09:19 +0200

    Started project

---
+++ src/components/NestedList.js
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import data from '../json.json';
+
+const NestedList = () => {
+  const [treeMultiline, setTreeMultiline] = useState(false);
+
+  return (
+    <>
+      <h1>zadanie rekrutacyjne</h1>
+      
+    </>
+  );
+};
+
+export default NestedList;
